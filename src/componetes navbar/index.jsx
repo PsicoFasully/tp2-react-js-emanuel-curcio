a1f13fcf02b47f1c5b4943384f630a4a57abe3e6
Emanuel Curcio <emanuelalejandro2021@hotmail.com>
2022-08-24 16:10:57 -0300

    tp2

---
+++ src/componetes navbar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const NavBar = () => {
+    return (
+        
+        <ul class ="lista">
+        <li><a href="">inicio</a></li>
+        <li><a href="">nosotros</a></li>
+        <li><a href="">carrito</a></li>
+        </ul>
+        
+    )
+}
+
+export default NavBar
